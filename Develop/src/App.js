5d0397cf99f34ca83f424cc9512bab30ea294311
Maria Ortega <mariaortegapb@gmail.com>
2021-03-14 11:22:39 -0400

    added title and wrapper components

--- Develop/src/App.js
+++ Develop/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
-import password from "./components/password";
+import Password from "./components/Password";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+
 class App extends Component {}
 
 export default App;
