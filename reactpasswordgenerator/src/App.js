d090a9e991e923cc8e43a9af87827d1883d2b63c
Maria Ortega <mariaortegapb@gmail.com>
2021-03-14 13:25:32 -0400

    added state to app.js

--- reactpasswordgenerator/src/App.js
+++ reactpasswordgenerator/src/App.js
@@ -5,23 +5,31 @@ import Title from "./components/Title";
 
 class App extends Component {}
     state = {
-        ///:///
-  };
+        choices: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=?",
+        password: ""
+      };
 
-  /// = id => {
+  createPassword = () => {
+
+var passwordCriteria = confirm("Would you like uppercase letters?"); 
+var passwordCriteria1 = confirm("Would you like lowercase letters?"); 
+var passwordCriteria2 = confirm("Would you like numbers?"); 
+var passwordCriteria3 = confirm("Would you like special characters (such as ! @ # ?) ]?"); 
+var passwordCriteria4 = prompt("For password lenght, please choose between 8-128 characters.")
     
-    const // = this.state.//.filter(// => ////.id !== id);
+    const choices = this.state.choices.filter(criteria => criteria === criteria);
     // Set this.state.// equal to the new friends array
-    this.setState({ // });
+    this.setState({ choices });
   };
 
   render() {
+    //render methods for generating password from the above state
     return (
       <Wrapper>
         <Title>Password Generator</Title>
-        {this.state.friends.map(friend => (
+        {this.state.choices.map(criteria) => (
           <Password
-            removeFriend={this.removeFriend}
+            createPassword={this.createPassword}
             id={friend.id}
             key={friend.id}
             name={friend.name}
