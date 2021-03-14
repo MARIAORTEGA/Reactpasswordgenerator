ad8e01c1e8f1c15a14fa0091111dd1d8eca47309
Maria Ortega <mariaortegapb@gmail.com>
2021-03-14 11:28:22 -0400

    added render() to app.js

--- Develop/src/App.js
+++ Develop/src/App.js
@@ -4,5 +4,35 @@ import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 
 class App extends Component {}
+    state = {
+        ///:///
+  };
+
+  /// = id => {
+    
+    const // = this.state.//.filter(// => ////.id !== id);
+    // Set this.state.// equal to the new friends array
+    this.setState({ // });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>Password Generator</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
 
 export default App;
