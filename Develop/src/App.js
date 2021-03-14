7122625169b50fe8b2dafb3cae2b04e1f38b9194
Maria Ortega <mariaortegapb@gmail.com>
2021-03-14 03:26:05 -0400

    started function for password component

--- Develop/src/App.js
+++ Develop/src/App.js
@@ -1,3 +1,5 @@
 import React, { Component } from "react";
+import password from "./components/password";
+class App extends Component {}
 
 export default App;
---
+++ Develop/src/components/password/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+
+function Password(props) {
+    return (
+
+
+        );
+    }       
+export default Password;
