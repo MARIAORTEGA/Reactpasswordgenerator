bf7dc1a586abc14d6d15c3f234ea0c006917ea0f
Maria Ortega <mariaortegapb@gmail.com>
2021-03-15 11:06:42 -0400

    added form

--- src/components/Options/index.js
+++ src/components/Options/index.js
@@ -2,18 +2,58 @@ import React from "react";
 
 function Options() {
   return (
-    <div class="form-check" align="center">
-      <input
-        class="form-check-input"
-        type="checkbox"
-        value=""
-        id="flexCheckDefault"
-      />
-      <label class="form-check-label" for="flexCheckDefault">
-        Default checkbox
-      </label>
-    </div>
+    <form>
+      <div class="form-check" align="center">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          value=""
+          id="flexCheckDefault"
+        />
+        <label class="form-check-label" for="flexCheckDefault">
+          Would you like uppercase letters?
+        </label>
+      </div>
+
+      <div class="form-check" align="center">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          value=""
+          id="flexCheckDefault"
+        />
+        <label class="form-check-label" for="flexCheckDefault">
+          Would you like lowercase letters?
+        </label>
+      </div>
+
+      <div class="form-check" align="center">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          value=""
+          id="flexCheckDefault"
+        />
+        <label class="form-check-label" for="flexCheckDefault">
+          Would you like numbers?
+        </label>
+      </div>
+
+      <div class="form-check" align="center">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          value=""
+          id="flexCheckDefault"
+        />
+        <label class="form-check-label" for="flexCheckDefault">
+          Would you like special characters (such as ! @ # ?)?
+        </label>
+      </div>
+    </form>
   );
 }
 
 export default Options;
+
+// http://react.tips/radio-buttons-in-reactjs/
